07c08d9c297f304fa5399f1bb3b60def72e1ff07
xiahangli <2427417167@qq.com>
2019-12-05 17:20:58 +0800

    dimen适配

--- sw_dimen/src/main/java/com/example/sw_dimen/generator/DimenGenerator.java
+++ sw_dimen/src/main/java/com/example/sw_dimen/generator/DimenGenerator.java
@@ -6,7 +6,11 @@ import com.example.sw_dimen.utils.MakeUtils;
 
 import java.io.File;
 
-
+/**
+ * 主程序类中的的静态变量先于静态代码块初始化，
+ * 其后进入主函数类(程序入口处main)，其后根据静态函数的调用情况，才能选择性的初始化。
+ * 生成dimens.xml的工具类
+ */
 public class DimenGenerator {
 
     /**
@@ -20,8 +24,8 @@ public class DimenGenerator {
     private static final int DESIGN_HEIGHT = 640;
 
     public static void main(String[] args) {
-        int smallest = DESIGN_WIDTH > DESIGN_HEIGHT ? DESIGN_HEIGHT : DESIGN_WIDTH;  //     求得最小宽度
-        DimenTypes[] values = DimenTypes.values();
+        int smallest = DESIGN_WIDTH > DESIGN_HEIGHT ? DESIGN_HEIGHT : DESIGN_WIDTH;  // 求得最小宽度,即宽和高的小值作为最小宽度
+        DimenTypes[] values = DimenTypes.values();//生成300-490共20个最小宽度的适配方案
         for (DimenTypes value : values) {
             File file = new File("./sw_dimen/");//生成的文件放在sw_dimen目录下
             MakeUtils.makeAll(smallest, value, file.getAbsolutePath());
--- sw_dimen/src/main/java/com/example/sw_dimen/utils/MakeUtils.java
+++ sw_dimen/src/main/java/com/example/sw_dimen/utils/MakeUtils.java
@@ -13,21 +13,45 @@ public class MakeUtils {
     private static final String XML_HEADER = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n";
     private static final String XML_RESOURCE_START = "<resources>\r\n";
     private static final String XML_RESOURCE_END = "</resources>\r\n";
-    private static final String XML_DIMEN_TEMPLETE = "<dimen name=\"qb_%1$spx_%2$d\">%3$.2fdp</dimen>\r\n";
+    /**
+     * 重点：按照比例生成转换后的值，
+     *
+     * jdk 格式化规范：%[argument_index$][flags][width][.precision]conversion 规范
+     */
+    private static final String XML_DIMEN_TEMPLETE = "<dimen name=\"dp_%1$spx_%2$d\">%3$.2fdp</dimen>\r\n";
 
-   
+
+    /**
+     * 格式化的String,用于写入<dimen name="qb_px_0">0.00dp</dimen>这样的字符串到文件中
+     */
     private static final String XML_BASE_DPI = "<dimen name=\"base_dpi\">%ddp</dimen>\r\n";
+    /**
+     * 文件中dp值最多到多少，如720代表支持0~720px的适配
+     */
     private  static final int MAX_SIZE = 720;
 
     /**
-     * 生成的文件名
+     * 生成的文件名，如dimens,dimen等xml文件
      */
     private static final String XML_NAME = "dimens.xml";
 
 
+    /**
+     *屏幕的宽度与横向dp的关系如下：
+     * px=dp*(dpi/160)=dp*（160的倍数）  【其中dpi是像素密度 dot per inch，如720*1280分辨率的手机，对角线为5.7inch的手机，则dpi=sqrt(720^2+1280^2)/5.7=257】
+     * 如小米5手机的px=1080，dpi=480 => dp=px/（dpi/160）=px/（160的倍数）=1080/3=360dp
+     * 故小米5的横向dp为360,那么android系统会找是否存在values-sw360dp的文件夹以及对应的资源文件。
+     *
+     * 上面160是数据mdpi的
+     * 像素转成dip格式，转换规则是px的值➗
+     * @param pxValue
+     * @param sw
+     * @param designWidth ui设计稿宽度如360dp
+     * @return 转换后的值，输出到dimens.xml文件中,eg：<dimen name="dp_px_1">0.83dp</dimen>中的0.83
+     */
     public static float px2dip(float pxValue, int sw,int designWidth) {
-        float dpValue =   (pxValue/(float)designWidth) * sw;
-        BigDecimal bigDecimal = new BigDecimal(dpValue);
+        float dpValue =   (pxValue/(float)designWidth) * sw;//核心，
+        BigDecimal bigDecimal = new BigDecimal(dpValue);//大数
         float finDp = bigDecimal.setScale(2, BigDecimal.ROUND_HALF_UP).floatValue();
         return finDp;
     }
@@ -37,6 +61,7 @@ public class MakeUtils {
      * 生成所有的尺寸数据
      *
      * @param type
+     * @param  designWidth 最小宽度，如360px
      * @return
      */
     private static String makeAllDimens(DimenTypes type, int designWidth) {
@@ -47,12 +72,13 @@ public class MakeUtils {
             sb.append(XML_HEADER);
             sb.append(XML_RESOURCE_START);
             //备份生成的相关信息
-            temp = String.format(XML_BASE_DPI, type.getSwWidthDp());
+            temp = String.format(XML_BASE_DPI, type.getSwWidthDp());//写入base_dpi信息，如<dimen name="base_dpi">300dp</dimen>
             sb.append(temp);
             for (int i = 0; i <= MAX_SIZE; i++) {
             	
-                dpValue = px2dip((float) i,type.getSwWidthDp(),designWidth);
-                temp = String.format(XML_DIMEN_TEMPLETE,"", i, dpValue);
+                dpValue = px2dip((float) i,type.getSwWidthDp(),designWidth);//按照将300-390区间的值与基准值比较，做px->dip转换
+               //格式化输出：
+                temp = String.format(XML_DIMEN_TEMPLETE,"", i, dpValue);//第一个是字符型1$s、第二个是整数型2$d、第三个是精度为小数点后2位数3$.2f
                 sb.append(temp);
             }
 
@@ -70,6 +96,7 @@ public class MakeUtils {
      * 生成的目标文件夹
      * 只需传宽进来就行
      *
+     * @param designWidth 最小的宽度 ，一般是UI设计稿的屏幕宽度，如360px
      * @param type 枚举类型
      * @param buildDir 生成的目标文件夹
      */
@@ -77,23 +104,23 @@ public class MakeUtils {
         try {
             //生成规则
             final String folderName;
-            if (type.getSwWidthDp() > 0) {
+            if (type.getSwWidthDp() > 0) {//swWidthDp范围在300~490
                 //适配Android 3.2+
                 folderName = "values-sw" + type.getSwWidthDp() + "dp";//文件夹的名字values-swxxxdp格式
             }else {
             	return;
             }
             
-            //生成目标目录
-            File file = new File(buildDir + File.separator + folderName);
+
+            File file = new File(buildDir + File.separator + folderName); //生成目标目录
             if (!file.exists()) {
                 file.mkdirs();
             }
 
             //生成values文件
-            FileOutputStream fos = new FileOutputStream(file.getAbsolutePath() + File.separator + XML_NAME);
-            fos.write(makeAllDimens(type,designWidth).getBytes());
-            fos.flush();
+            FileOutputStream fos = new FileOutputStream(file.getAbsolutePath() + File.separator + XML_NAME);//生成目标目录
+            fos.write(makeAllDimens(type,designWidth).getBytes());//
+            fos.flush();//一次性将缓冲区的数据写到文件中
             fos.close();
 
 
