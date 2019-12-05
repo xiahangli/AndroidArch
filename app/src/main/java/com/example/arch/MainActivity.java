cf93828b149b211ccc0983f78c11381f3a576129
xiahangli <2427417167@qq.com>
2019-12-05 17:18:50 +0800

    1像素的保活方案实践,启动Service 注册1像素activity

--- app/src/main/java/com/example/arch/MainActivity.java
+++ app/src/main/java/com/example/arch/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.arch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.arch.onepixel.OnePixelService;
+
+/**
+ * 入口app
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startOnePixelService();
+    }
+
+    private void startOnePixelService() {
+        Intent intent = new Intent(MainActivity.this, OnePixelService.class);
+        startService(intent);
     }
 }
